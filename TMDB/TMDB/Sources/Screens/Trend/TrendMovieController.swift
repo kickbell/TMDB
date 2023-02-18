7efcf7a935aff5074a1b7c177c34e8d8c4113136
kickbell <dev.kickbell@gmail.com>
2023-02-18 23:34:25 +0900

    탭바 컨트롤러 설정

---
+++ TMDB/TMDB/Sources/Screens/Trend/TrendMovieController.swift
@@ -0,0 +1,29 @@
+//
+//  TrendMovieController.swift
+//  TMDB
+//
+//  Created by jc.kim on 2/18/23.
+//
+
+import UIKit
+
+class TrendMovieController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
